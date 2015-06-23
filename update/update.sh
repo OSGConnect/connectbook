baaea0cad9067e81f8613b6400e2222bf3457988
David Champion <dgc@uchicago.edu>
2015-06-23 16:37:06 -0500

    Write links to upstream sources into html output

--- update/update.sh
+++ update/update.sh
@@ -6,14 +6,17 @@ echo child pid: $$
 echo dir: $(pwd)
 echo files: "$@"
 
-git checkout master
-git pull origin master
-git submodule foreach git pull origin master
+#git checkout master
+#git pull origin master
+#git submodule foreach git pull origin master
 
 #for file in "$@"; do
 #	$(dirname $0)/freshpush $(pwd)/$file
 #done
 
 find . -name \*.md -print | while read file; do
-	$(dirname $0)/freshpush $(pwd)/$file
+	dir=$(dirname "$file")
+	upstream=$(cd "$dir"; git config --get remote.origin.url | sed -e 's/.git$//')
+	relpath=$(cd "$dir"; git ls-files --full-name $(basename "$file"))
+	env "upstream=$upstream" "relpath=$relpath" $(dirname $0)/freshpush $(pwd)/$file
 done
